2857bfe4ee57555986474307e261452fc9dbce15
Brenton Kenkel <brenton.kenkel@gmail.com>
2025-05-28 18:50:44 -0500

    Add nvim-cmp
    Also remove the lazy-lock file that I accidentally had included

---
+++ dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,48 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+  },
+  config = function()
+    local cmp = require("cmp")
+    cmp.setup({
+      completion = {
+        autocomplete = false
+      },
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      sources = cmp.config.sources({
+        { name = "luasnip", max_item_count = 2 },
+        { name = "nvim_lsp", max_item_count = 2 },
+        { name = "luasnip", max_item_count = 2 },
+        { name = "buffer", max_item_count = 2 },
+        { name = "path", max_item_count = 2 },
+      }),
+      mapping = {
+        ['<C-p>']     = cmp.mapping.select_prev_item(),
+        ['<C-n>']     = cmp.mapping.select_next_item(),
+        ['<S-Tab>']   = cmp.mapping.select_prev_item(),
+        ['<Tab>']     = cmp.mapping.select_next_item(),
+        ['<C-S-f>']   = cmp.mapping.scroll_docs(-4),
+        ['<C-f>']     = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>']     = cmp.mapping.close(),
+        ['<C-m>']     = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Insert,
+          select   = true,
+        }),
+        ['<CR>']      = cmp.mapping.confirm({select = false}),
+      },
+      experimental = {
+        ghost_text = false,
+      },
+    })
+  end,
+}
