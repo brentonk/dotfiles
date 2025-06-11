478c4fa32148bafab69d31b1a70ba0af80f54c8c
Brenton Kenkel <brenton.kenkel@gmail.com>
2025-06-11 15:32:22 -0500

    More annoying tab fixes

--- dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,55 +1,72 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "L3MON4D3/LuaSnip",
-    "lukas-reineke/cmp-rg",
-    "micangl/cmp-vimtex",
-    "saadparwaiz1/cmp_luasnip",
-  },
-  config = function()
-    local cmp = require("cmp")
-    cmp.setup({
-      completion = {
-        autocomplete = false
-      },
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body)
-        end,
-      },
-      sources = cmp.config.sources({
-        { name = "vimtex" },
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "rg" },
-        { name = "path" },
-      }, {
-        { name = "buffer" }
-      }),
-      mapping = {
-        ['<C-p>']     = cmp.mapping.select_prev_item(),
-        ['<C-n>']     = cmp.mapping.select_next_item(),
-        ['<S-Tab>']   = cmp.mapping.select_prev_item(),
-        ['<Tab>']     = cmp.mapping.select_next_item(),
-        ['<Down>'] = cmp.mapping.select_next_item(),
-        ['<Up>']     = cmp.mapping.select_prev_item(),
-        ['<C-S-f>']   = cmp.mapping.scroll_docs(-4),
-        ['<C-f>']     = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<M-Space>'] = cmp.mapping.complete(),
-        ['<C-e>']     = cmp.mapping.close(),
-        ['<C-m>']     = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Insert,
-          select   = true,
-        }),
-        ['<CR>']      = cmp.mapping.confirm({select = false}),
-      },
-      experimental = {
-        ghost_text = false,
-      },
-    })
-  end,
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"L3MON4D3/LuaSnip",
+		"lukas-reineke/cmp-rg",
+		"micangl/cmp-vimtex",
+		"saadparwaiz1/cmp_luasnip",
+	},
+	config = function()
+		local cmp = require("cmp")
+    local luasnip = require("luasnip")
+		cmp.setup({
+			completion = {
+				autocomplete = false,
+			},
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			sources = cmp.config.sources({
+				{ name = "vimtex" },
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "rg" },
+				{ name = "path" },
+			}, {
+				{ name = "buffer" },
+			}),
+			mapping = {
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<Down>"] = cmp.mapping.select_next_item(),
+				["<Up>"] = cmp.mapping.select_prev_item(),
+				["<C-S-f>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<M-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.close(),
+				["<C-m>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Insert,
+					select = true,
+				}),
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			},
+			experimental = {
+				ghost_text = false,
+			},
+		})
+	end,
 }
