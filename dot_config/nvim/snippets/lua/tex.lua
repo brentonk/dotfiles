f8e03aeb01a256e2b55d2ac8e2c2d11b5d235148
Brenton Kenkel <brenton.kenkel@gmail.com>
2025-06-24 21:37:18 -0500

    Add MoveEqLeft snippet

--- dot_config/nvim/snippets/lua/tex.lua
+++ dot_config/nvim/snippets/lua/tex.lua
@@ -1,27 +1,46 @@
 return {
-      s({ trig = "res", dscr = "Restatable environment" },
-        fmta(
-          [[
+	s(
+		{ trig = "res", dscr = "Restatable environment" },
+		fmta(
+			[[
           \begin{restatable}{<>}{res<>}
             \label{res:<>}
             <>
           \end{restatable}
           ]],
-          {
-            i(1, "environment"),
-            i(2, "CommandName"),
-            f(
-              function(args)
-                -- ChatGPT-written function for CamelCase -> kebab-case
-                return args[1][1]
-                    :gsub("(%l)(%u)", "%1-%2") -- fooBar → foo-Bar
-                    :gsub("(%u)(%u%l)", "%1-%2") -- HTMLParser → HTML-Parser
-                    :lower()
-              end,
-              { 2 }
-            ),
-            i(0, "body")
-          }
-        )
-      ),
+			{
+				i(1, "environment"),
+				i(2, "CommandName"),
+				f(function(args)
+					-- ChatGPT-written function for CamelCase -> kebab-case
+					return args
+						[1]
+						[1]
+						:gsub("(%l)(%u)", "%1-%2") -- fooBar → foo-Bar
+						:gsub("(%u)(%u%l)", "%1-%2") -- HTMLParser → HTML-Parser
+						:lower()
+				end, { 2 }),
+				i(0),
+			}
+		)
+	),
+	s(
+		{ trig = "eq", dscr = "Equation environment" },
+		fmta(
+			[[
+          \begin{equation}
+            \label{eq:<>}
+            <>
+          \end{equation}
+        ]],
+			{
+				i(1, "label"),
+				i(0),
+			}
+		)
+	),
+	s({ trig = "mel", dscr = "MoveEqLeft" }, {
+		t("\\MoveEqLeft{}"),
+		i(0),
+	}),
 }
