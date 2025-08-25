c02fd11d4347d66b5ee3557b096d54ca31259604
Brenton Kenkel <brenton.kenkel@gmail.com>
2025-08-25 14:07:13 -0500

    More changes for Mac environment

--- dot_config/nvim/lua/plugins/vimtex.lua
+++ dot_config/nvim/lua/plugins/vimtex.lua
@@ -3,7 +3,12 @@ return {
   lazy = false,
   ft = "tex",
   config = function()
-    vim.g.vimtex_view_method = "zathura" -- Use Zathura on Linux (adjust for macOS)
+    -- Platform-specific PDF viewer setup
+    if vim.fn.has('mac') == 1 then
+      vim.g.vimtex_view_method = "skim"
+    else
+      vim.g.vimtex_view_method = "zathura"
+    end
     vim.g.vimtex_compiler_method = "latexmk"
     vim.g.vimtex_quickfix_open_on_warning = 0
     vim.g.vimtex_indent_lists = {} -- disable weird hanging indent in lists
