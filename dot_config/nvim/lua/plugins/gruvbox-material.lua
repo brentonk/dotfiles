dffed1dca0fc1aa0436ae6ea180cd41e02d94e75
Brenton Kenkel <brenton.kenkel@gmail.com>
2025-09-27 11:04:01 -0500

    Install catppuccin themes

--- dot_config/nvim/lua/plugins/gruvbox-material.lua
+++ dot_config/nvim/lua/plugins/gruvbox-material.lua
@@ -1,12 +1,12 @@
 return {
   'sainnhe/gruvbox-material',
-  lazy = false,
-  priority = 1000,
+  lazy = true, -- Load on demand instead of automatically
   config = function()
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
     vim.g.gruvbox_material_enable_italic = true
     vim.g.gruvbox_material_transparent_background = 1
-    vim.cmd.colorscheme('gruvbox-material')
+    -- Removed automatic colorscheme setting to allow other themes as default
+    -- To use: :colorscheme gruvbox-material
   end
 }
