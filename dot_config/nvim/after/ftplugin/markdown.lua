8438efcbabe4cd5553f505dcf6105f5476245167
Brenton Kenkel <brenton.kenkel@gmail.com>
2026-01-15 14:41:03 -0600

    Ensure tabwidth of 2 in markdown

--- dot_config/nvim/after/ftplugin/markdown.lua
+++ dot_config/nvim/after/ftplugin/markdown.lua
@@ -2,3 +2,9 @@ vim.opt_local.conceallevel = 0
 
 -- Align wrapped lines with list item text
 vim.opt_local.breakindentopt = "list:-1"
+
+-- Tab width settings (override obsidian.nvim defaults)
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.expandtab = true
