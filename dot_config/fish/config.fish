34b307c4e9048c139caf307cc56adb63767a21f1
Brenton Kenkel <brenton.kenkel@gmail.com>
2026-01-23 15:41:44 -0600

    Use fnm (install via cargo)

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -4,12 +4,16 @@ if status is-login
 
   contains $HOME/.local/bin $PATH
   or set PATH $HOME/.local/bin $PATH
+
+  contains $HOME/.cargo/bin $PATH
+  or set PATH $HOME/.cargo/bin $PATH
 end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
     starship init fish | source
+    fnm env --use-on-cd | source
 end
 
 # Set editor to nvim
