edb061ad5d3d1ca5300a0b4ab236c86a09a6df11
Brenton Kenkel <brenton.kenkel@gmail.com>
2026-03-17 14:02:44 -0500

    Fix keychain.fish: use local var instead of appending to universal var every login

--- dot_config/fish/conf.d/keychain.fish
+++ dot_config/fish/conf.d/keychain.fish
@@ -1,4 +1,4 @@
 if status is-login; and status is-interactive
-  set -Ua KEYS_TO_AUTOLOAD ~/.ssh/id_ed25519
-  keychain --eval --quiet -Q $KEYS_TO_AUTOLOAD | source
+  set -l keys ~/.ssh/id_ed25519
+  keychain --eval --quiet -Q $keys | source
 end
