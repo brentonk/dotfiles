490f685fd704998abc20f53d403b472b58d9ba00
Brenton Kenkel <brenton.kenkel@gmail.com>
2026-03-30 17:27:00 -0500

    Fix Oil keybinds: move to lazy.nvim keys for proper lazy-loading

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,12 +1,11 @@
 return {
 	"stevearc/oil.nvim",
-	config = function()
-		require("oil").setup({
-    })
-		vim.keymap.set("n", "<leader>Oo", "<cmd>Oil<cr>", { desc = "Open Oil" })
-		vim.keymap.set("n", "<leader>Of", "<cmd>Oil --float<cr>", { desc = "Open Oil in floating window" })
-	end,
+	opts = {},
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	lazy = true,
 	cmd = { "Oil" },
+	keys = {
+		{ "<leader>Oo", "<cmd>Oil<cr>", desc = "Open Oil" },
+		{ "<leader>Of", "<cmd>Oil --float<cr>", desc = "Open Oil in floating window" },
+	},
 }
