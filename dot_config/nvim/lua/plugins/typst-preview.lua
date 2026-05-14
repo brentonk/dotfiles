2fdbe9f251874b212e217c246db930b0a8aad3c7
Brenton Kenkel <brenton.kenkel@gmail.com>
2026-05-14 06:45:40 -0500

    Open typst preview in chromeless Brave app window

--- dot_config/nvim/lua/plugins/typst-preview.lua
+++ dot_config/nvim/lua/plugins/typst-preview.lua
@@ -2,5 +2,7 @@ return {
   "chomosuke/typst-preview.nvim",
   ft = "typst",
   version = "1.*",
-  opts = {},
+  opts = {
+    open_cmd = "brave --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform-hint=auto --app=%s",
+  },
 }
