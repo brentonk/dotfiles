d04e24a29b4c4aeff7be0e606f88fc43eb10039b
Brenton Kenkel <brenton.kenkel@gmail.com>
2026-06-03 12:41:49 -0500

    Revert "Add qp fish function: quarto preview on 127.0.0.1:4848"
    This reverts commit 465b0593a7c9b2f21b15185a5eaa401a4648adb2.

--- dot_config/fish/functions/qp.fish
+++
@@ -1,6 +0,0 @@
-function qp --wraps='quarto preview' --description 'quarto preview bound to 127.0.0.1 (avoids localhost browser hang on office desktop)'
-  # Force the IPv4 literal instead of `localhost`: Chrome on the office
-  # desktop hangs/spins when pointed at http://localhost:<port>, but loads
-  # http://127.0.0.1:<port> instantly. See ~/.config/OFFICE_COMPUTER_BROWSER_HANG.md
-  quarto preview --host 127.0.0.1 --port 4848 $argv
-end
